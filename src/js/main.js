4acfc78aac6952eb77e2334aebe447cd07dd5564
Patrick Bower <bower.patrick@gmail.com>
2017-03-14 17:01:36 +0000

    Added libraries (#3)
    Bootstrap SASS
    jQuery

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
+// libs
+import $ from "jquery";
+
+// mods
 import one from './hello';
 import two from './world';
 
+// tests
 one();
 two();
+
+$('document').ready(function(){
+  $('h1').css('background', 'blue');
+})
