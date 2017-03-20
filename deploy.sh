c154b95dcc78e64bcda715c8d609cc32370617a8
Patrick Bower <bower.patrick@gmail.com>
2017-03-20 16:34:25 +0000

    added npm scripts to run and deploy (#2)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Grunt
+grunt
 git commit -am "Commit changes"
 git checkout -B gh-pages
 git add -f build
@@ -8,4 +8,4 @@ git commit -am "Run build"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
 git checkout -
-Grunt
+grunt
